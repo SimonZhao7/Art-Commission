9e572e3911ed184b5608967add994feeab51b7b5
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-07-01 16:18:36 -0700

    Add Message to DB

--- app/message/[id]/page.tsx
+++ app/message/[id]/page.tsx
@@ -7,11 +7,16 @@ import {
   collection,
   where,
   getDoc,
+  addDoc,
   doc,
+  serverTimestamp,
 } from "firebase/firestore";
 import { db } from "@/firebase";
 // Hooks
 import { useAuth } from "@/hooks/useFirebaseUser";
+import { SubmitHandler, useForm } from "react-hook-form";
+// Icons
+import { AiOutlineSend } from "react-icons/ai";
 
 interface Props {
   params: {
@@ -34,8 +39,13 @@ interface User {
   profileImage: string;
 }
 
+type TextMsgInput = {
+  message: string;
+};
+
 export default function Chat({ params }: Props) {
   const [messages, setMessages] = useState<Message[]>([]);
+  const { register, handleSubmit } = useForm<TextMsgInput>();
   const currentUser = useAuth();
 
   useEffect(() => {
@@ -61,12 +71,26 @@ export default function Chat({ params }: Props) {
     return () => unsubscribe();
   }, []);
 
+  const onTextMsgSubmit: SubmitHandler<TextMsgInput> = async (data, e) => {
+    const { message } = data;
+    e?.preventDefault();
+    if (message.length > 0 && currentUser != null) {
+      await addDoc(collection(db, "messages"), {
+        chatId: params.id,
+        message,
+        messageType: "TEXT",
+        senderId: currentUser.uid,
+        timestamp: serverTimestamp(),
+      });
+    }
+  };
+
   return (
     <section className="flex-[2] p-10 relative">
       {messages.map((msg) => (
         <div
           key={msg.id}
-          className={`flex ${
+          className={`flex mb-4 ${
             msg.sender.id === currentUser?.uid && "flex-row-reverse"
           }`}
         >
@@ -79,7 +103,7 @@ export default function Chat({ params }: Props) {
           />
           <div
             key={msg.id}
-            className={`bg-light-gray rounded-[20px] py-3 px-6 inline-flex flex-wrap max-w-[50%] ${
+            className={`bg-light-gray rounded-[20px] py-3 px-4 inline-flex flex-wrap max-w-[50%] ${
               msg.sender.id === currentUser?.uid && "float-right"
             }`}
           >
@@ -91,9 +115,18 @@ export default function Chat({ params }: Props) {
           </div>
         </div>
       ))}
-      <div className="absolute w-full left-0 bottom-0 p-3">
-        <textarea className="border-2 w-full border-light-gray resize-none outline-none text-sm p-3"></textarea>
-      </div>
+      <form
+        className="absolute flex items-end w-full gap-2 left-0 bottom-0 p-3"
+        onSubmit={handleSubmit(onTextMsgSubmit)}
+      >
+        <textarea
+          {...register("message")}
+          className="border-2 flex-1 border-light-gray resize-none outline-none text-sm p-3"
+        ></textarea>
+        <button type="submit" className="py-2">
+          <AiOutlineSend className="text-black w-5 h-5" />
+        </button>
+      </form>
     </section>
   );
 }
