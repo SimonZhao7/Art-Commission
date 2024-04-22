cec1af734cf165d4dd44cfb760eb4cb6e43ca986
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2024-04-21 18:02:32 -0700

    Hack textarea to resize as new lines are added

--- app/message/[id]/MessageForm.tsx
+++ app/message/[id]/MessageForm.tsx
@@ -28,8 +28,6 @@ export default function MessageForm({ id, openImageModal }: Props) {
         senderId: currentUser.id,
         timestamp: serverTimestamp(),
       });
-      const el = document.getElementById("textarea");
-      el!.innerHTML = "";
       setMessage("");
     }
   };
@@ -42,16 +40,19 @@ export default function MessageForm({ id, openImageModal }: Props) {
       <button className="py-2 relative" onClick={openImageModal}>
         <FaCirclePlus className="text-dark-gray w-5 h-5" />
       </button>
-      <span
-        id="textarea"
-        onInput={(e) => {
-          const node = e.target as Element;
-          setMessage(node.innerHTML!);
-        }}
-        placeholder="Message..."
-        className="block w-full text-dark-gray bg-transparent font-montserrat outline-none overflow-scroll text-sm max-h-[76px] px-1"
-        contentEditable={true}
-      ></span>
+      <div
+        className={
+          "grid w-full text-dark-gray font-montserrat overflow-scroll text-sm max-h-[76px] px-1 after:content-[attr(content)'_'] after:whitespace-pre-wrap after:invisible after:row-start-1 after:row-end-2 after:col-start-1 after:col-end-2"
+        }
+        content={message}
+      >
+        <textarea
+          rows={1}
+          className="resize-none overflow-hidden bg-transparent outline-none row-start-1 row-end-2 col-start-1 col-end-2"
+          value={message}
+          onChange={(e) => setMessage(e.currentTarget.value)}
+        ></textarea>
+      </div>
       <button type="submit" className="py-2 relative">
         <IoSend className="text-dark-gray w-5 h-5" />
       </button>
